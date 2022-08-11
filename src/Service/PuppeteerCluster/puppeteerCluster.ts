a0cfe08ee9889c54c417c8c280e10ef82901b450
jonrc <jonarc06@gmail.com>
2022-08-11 10:05:59 -0300

    update puppetteer cluster

--- src/Service/PuppeteerCluster/puppeteerCluster.ts
+++ src/Service/PuppeteerCluster/puppeteerCluster.ts
@@ -1,13 +1,21 @@
 import { Cluster } from 'puppeteer-cluster'
 
-export const clusterInstance = Cluster.launch({
-  concurrency: Cluster.CONCURRENCY_BROWSER,
-  maxConcurrency: 5,
-  puppeteerOptions: {
-    headless: true,
-    args: ['--no-sandbox']
-  },
-  timeout: 60000
-})
+const createClusterInstance = () =>
+  Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_BROWSER,
+    maxConcurrency: 2,
+    puppeteerOptions: {
+      headless: true,
+      args: ['--no-sandbox']
+    },
+    timeout: 60000
+  })
+
+export const clusterInstance = createClusterInstance()
+  .then(() => createClusterInstance())
+  .catch(error => {
+    console.error(error)
+    return createClusterInstance()
+  })
 
 export const puppeteerCluster = () => clusterInstance
